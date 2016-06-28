1976d5d41961fe167bef00b65cb480fa3c09c865
HysteriaShoot <hysteriashoot@gmail.com>
2016-06-28 20:51:02 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function twitterOver(){
+	document.getElementById('iconoTwitter').src = "./media/header/twitterColor.png";
+}
+function twitterOut(){
+	document.getElementById('iconoTwitter').src = "./media/header/twitterBW.png";
+}

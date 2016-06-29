851e140fadb98b051644486e4e798572fbddc659
HysteriaShoot <hysteriashoot@gmail.com>
2016-06-29 21:55:35 +0200

    Delete index.js

--- js/index.js
+++
@@ -1,6 +0,0 @@
-function twitterOver(){
-	document.getElementById('iconoTwitter').src = "./media/header/twitterColor.png";
-}
-function twitterOut(){
-	document.getElementById('iconoTwitter').src = "./media/header/twitterBW.png";
-}

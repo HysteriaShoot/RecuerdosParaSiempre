5736af41ec23b5607675b148772b2b6aca08f96e
HysteriaShoot <hysteriashoot@gmail.com>
2017-06-08 16:32:01 +0200

    First Upload
    First files upload

---
+++ js/carrusel.js
@@ -0,0 +1,36 @@
+window.addEventListener ('load',intervaloCarrusel);
+var i = 1;
+
+function intervaloCarrusel(){
+	setInterval('carrusel()',2500);
+}
+
+function stopInterval(){
+	clearInterval(interval);
+}
+
+function carrusel(){
+	if (i < 5){
+		document.getElementById('imageCarrusel').src ="../media/carrusel/foto" + i + ".jpg";
+		i++;
+	}else{
+		i = 1;
+		carrusel();
+	}
+}
+
+function carruselNext(){
+	i++;
+	if(i > 6){
+		i = 1;
+	}
+	document.getElementById('imageCarrusel').src ="../media/carrusel/foto" + i + ".jpg";
+}
+
+function carruselPrev(){
+	i--;
+	if(i < 1){
+		i = 6;
+	}
+	document.getElementById('imageCarrusel').src ="../media/carrusel/foto" + i + ".jpg";
+}
---
+++ js/index.js
@@ -0,0 +1,14 @@
+function cambio(n){
+  if(n==1){
+    document.getElementById('ventana').src="./html/carrusel.html";
+  }
+  if(n==2){
+    document.getElementById('ventana').src="./html/carrusel.html";
+  }
+  if(n==3){
+    document.getElementById('ventana').src="./html/carrusel.html";
+  }
+  if(n==4){
+    document.getElementById('ventana').src="./html/contacto.html";
+  }
+}
